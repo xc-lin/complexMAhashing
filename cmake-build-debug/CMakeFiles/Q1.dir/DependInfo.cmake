584c90684defbb6a849f102e68151952b4415b05
xc-lin <736196315@qq.com>
2021-05-11 12:12:32 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/Q1.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/a606/CLionProjects/unsw/comp9315/FinalExam/complexMAhashing-Q1/bits.c" "/Users/a606/CLionProjects/unsw/comp9315/FinalExam/complexMAhashing-Q1/cmake-build-debug/CMakeFiles/Q1.dir/bits.c.o"
+  "/Users/a606/CLionProjects/unsw/comp9315/FinalExam/complexMAhashing-Q1/hash.c" "/Users/a606/CLionProjects/unsw/comp9315/FinalExam/complexMAhashing-Q1/cmake-build-debug/CMakeFiles/Q1.dir/hash.c.o"
+  "/Users/a606/CLionProjects/unsw/comp9315/FinalExam/complexMAhashing-Q1/pages.c" "/Users/a606/CLionProjects/unsw/comp9315/FinalExam/complexMAhashing-Q1/cmake-build-debug/CMakeFiles/Q1.dir/pages.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Q1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Q1.dir/bits.c.o"
+  "CMakeFiles/Q1.dir/hash.c.o"
+  "CMakeFiles/Q1.dir/pages.c.o"
+  "Q1"
+  "Q1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Q1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
